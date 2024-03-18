44ca4a8704066592bf2ba402e0bc919e51d16f78
Marks Kalijevs <markskalijevs@gmail.com>
2024-03-18 16:04:41 +0200

    Fix typo in console.php routes

--- laravel-backend/routes/console.php
+++ laravel-backend/routes/console.php
@@ -14,5 +14,5 @@ Artisan::command('updaterates', function () {
     $controller = new CurrencyRateController;
     $controller->store();
 })->purpose('Updates the exchange rates')->dailyAt('8:00');
-// })->purpose('Updates the exchange rates')->->everyFifteenSeconds();
+// })->purpose('Updates the exchange rates')->everyFifteenSeconds();
 // Comment the previous line and uncomment the last for the sake of testing (to populate DB with data within a minute-two instead of days)
