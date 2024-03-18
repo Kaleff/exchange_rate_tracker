0744ac1f230ad44f9b63102ad00831dc06dbc249
Marks Kalijevs <markskalijevs@gmail.com>
2024-03-18 03:11:24 +0200

    Add avg, min and max to rates table

--- react-frontend/src/components/rates.tsx
+++ react-frontend/src/components/rates.tsx
@@ -15,18 +15,31 @@ export default function Rates() {
   const [rates, setRates] = useState<Rate[]>([]);
   const [errors, setErrors] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [ascOrder, setAscOrder] = useState(true);
   // Pagination elements
   const currentItems = rates.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(rates.length / 10);
-
+  // Min, max, average
+  const ratesMap = rates.map(item => item.exchange_rate);
+  const ratesMax = Math.max(...ratesMap);
+  const ratesMin = Math.min(...ratesMap);
+  const ratesAvg = (ratesMap.reduce((a, b) => a + b, 0) / ratesMap.length).toFixed(5);;
+  // Handle the page change
   const handlePageClick = (event : PaginationEvent) => {
     const newOffset = (event.selected * 10) % rates.length;
     setItemOffset(newOffset);
   };
 
+  const switchOrder = () => {
+    !ascOrder ? setRates(rates.sort((a,b) => a.id - b.id)) : setRates(rates.sort((a,b) => b.id - a.id))
+    setAscOrder(!ascOrder);
+    console.log(rates);
+  }
+  // Fetch currency data
   useEffect(() => {
     setRates([]);
     setLoading(true);
+    setAscOrder(true);
     getRates(currency as Currency)
       .then((response) => {
         setErrors([]);
@@ -47,7 +60,10 @@ export default function Rates() {
         <table>
           <thead>
             <tr>
-              <th>Date</th>
+              <th className="clickable" onClick={() => switchOrder()}>Date
+                {ascOrder && <svg data-name="1-Arrow Up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width='25px' height='25px'><path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"/></svg>}
+                {!ascOrder && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width='25px' height='25px'><path d="m18.294 16.793-5.293 5.293V1h-1v21.086l-5.295-5.294-.707.707L12.501 24l6.5-6.5-.707-.707z"/></svg>}
+              </th>
               <th>EUR to {currency}</th>
             </tr>
           </thead>
@@ -63,6 +79,11 @@ export default function Rates() {
           })}
         </table>
       )}
+      {!loading && <>
+        <p>Minimum: {ratesMin} {currency}</p>
+        <p>Maximum: {ratesMax} {currency}</p>
+        <p>Average: {ratesAvg} {currency}</p>
+      </>}
       <SyncLoader loading={loading} size={15} color="#646cff" />
       <ReactPaginate
         breakLabel="..."
